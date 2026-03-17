b53eb8415932deee2150e0fe471570a7a2d38e18
Bob Zhang <zz347@duke.edu>
2026-03-16 21:16:35 -0400

    additional hws

---
+++ src/Class0HW.java
@@ -0,0 +1,11 @@
+public class Class0HW {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        int result = multiply(x,y);
+        System.out.println(result);
+    }
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+}
---
+++ src/MyHelloWorld.java
@@ -0,0 +1,8 @@
+public class MyHelloWorld {
+    public static void main(String[] args) {
+        int x = -5;
+        int y = 3;
+        String s = "abc" + 1 + "!!!";
+        System.out.println(s);
+    }
+}
